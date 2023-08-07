e61ce1adb8ece9af62e7d53081c68efc6d67b1ba
Oscar <49494970+ostevenvg@users.noreply.github.com>
2023-08-07 12:37:41 -0600

    Update asoarcosQB.bat

--- pagos/scripts/asoarcosQB.bat
+++ pagos/scripts/asoarcosQB.bat
@@ -1,3 +1,4 @@
-call activate asoarcos
-python scripts/gui.py
-call conda deactivate
+set conda_dir="C:\Users\villalta\AppData\Local\miniconda3"
+call %conda_dir%\condabin\_conda_activate.bat asoarcos
+set path=%conda_dir%\envs\asoarcos\;%path%
+python "C:\Users\villalta\Documents\Personal\repos\asoarcos\pagos\scripts\gui.py"
